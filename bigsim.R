b346f3ed013106145a5e100a4cdba2063438fc16
Jeepen <jeppe.ekstrand.halkjaer@gmail.com>
2022-10-04 10:42:42 +0200

    first commit

---
+++ bigsim.R
@@ -0,0 +1,80 @@
+rm(list=ls())
+set.seed(13072020)
+library(survival)
+library(data.table)
+library(Matrix)
+library(doMC)
+library(doRNG)
+registerDoMC(6)
+source("~/Dropbox/phd/Project2/R/estimation.R")
+
+# Parametre -------------------------------------------------------------------------------------------------------
+n <- 1e3                                                  # Antal obs
+lambda <- 1e-4                                            # Konstant baseline hazard 
+HR <- 1                                                   # HR af behandling
+HRsex <- 2
+tau <- rexp(n, rate=1/1000)                               # Censorering
+## tau <- 1000
+lambdaPois <- 2                                           # Parameter til poissonfordeling
+D <- 100                                                  # Behandlingslængde
+M <- 4
+
+## Simulation
+starttime <- Sys.time()
+ests <- foreach(i = 1:100, .options.RNG = 27102020, .combine = "rbind") %dorng% {
+  cat("outer iteration: ", i, "\n")
+  W <- rpois(n, lambda = lambdaPois) + 1                         # Antal behandlinger
+  UW <- (W == 1) + 2 * (W == 2) + 3 * (W == 3)+ 4 * (W >= 4)     # HR for forskellige antal behandlinger
+  sex <- rbinom(n, 1, .5)
+  u <- runif(n)
+  br <- 1 - exp(-lambda * W * D * UW * HR * HRsex^sex)
+  X <- numeric(n)
+  X[u < br] <- -log(1 - u[u < br]) / (lambda * HR * UW[u < br] * HRsex^sex[u < br])
+  X[u >= br] <- (-log(1 - u[u >= br]) + lambda * UW[u >= br] * HRsex^sex[u >= br] * W[u >= br] * D * (1 - HR)) /
+    (lambda * UW[u >= br] * HRsex^sex[u >= br])
+  tau <- rexp(n, 1/1000)                                         # Censorering
+  T <- pmin(X, tau)
+  status <- as.numeric(X <= tau)
+  obsW <- pmin(W, ceiling(T / D))                                # Vi observerer ikke W for alle
+  treat <- as.numeric(T <= (D * W))                              # Behandling eller ej ved event tidspunkt
+  statusW <- 1-treat                                             # Fortæller os om vi observerer W
+  statusW[obsW >= M] <- 1                                        # Vi kender niveau hvis de behandles 4 eller flere gange
+  tmp <- do.call("rbind", lapply(1:n, function(i){
+    if(treat[i] == 1) data.table(id = i, time1 = 0, time2 = T[i], status = status[i], treat = 1, W = min(obsW[i],M),
+                                 statusW = statusW[i], sex = sex[i])
+    else data.table(id = i, time1 = c(0, D*W[i]), time2 = c(D*W[i], T[i]), status = c(0, status[i]),
+                    treat = c(1,0), W = min(obsW[i],M), statusW = statusW[i], sex = sex[i])
+  }))
+  cox1 <- coxph(Surv(time1,time2,status) ~ treat + sex, data = tmp)
+  cox2 <- coxph(Surv(time1,time2,status) ~ treat + strata(factor(W)) + sex, data = tmp)
+  model <- em(Surv(time1,time2,status) ~ treat + strata(factor(W)) + sex, data = tmp, M = M)
+  ests <- coef(model$model)[1]
+  SE <- emVar(model)[1]
+  cat("estimate", i, ": ", ests, "\n")
+  cat("standard error", i, ": ", SE, "\n")
+  nn <- floor((T-1e-10) / D)
+  tmp2 <- do.call("rbind", lapply(1:n, function(i){
+    sek <- seq(0, D * nn[i], D)
+    data.table(id = i, time1 = sek, time2 = c(sek[-1], T[i]), status = c(rep(0, length(sek)-1), status[i]),
+               sex = sex[i], treat = c(rep(1, obsW[i]), rep(0, length(sek) - obsW[i])), W = c(1:obsW[i], rep(obsW[i], length(sek) - obsW[i])))
+  }))
+  tmp2$W[tmp2$W >= M] <- M
+  cox3 <- coxph(Surv(time1,time2,status) ~ treat + sex + strata(factor(W)), data = tmp2)
+  cat("newCox", i, ": ", coef(cox3)[1], "\n")
+  c(ests, SE, coef(cox1)[1], coef(cox2)[1], coef(cox3)[1])
+  ## c(ests, SE, coef(cox1)[1], coef(cox2)[1])
+  ## c(i, ests)
+}
+endtime <- Sys.time()
+difftime(endtime,starttime)
+mean(ests[,1])
+exp(mean(ests[,1]))
+mean(ests[,2])
+sd(ests[,1])
+mean(ests[,ncol(ests)])
+exp(mean(ests[,ncol(ests)]))
+mean(ests[,1]-1.96*ests[,2]<log(2) & ests[,1]+1.96*ests[,2]>log(2))
+sum(is.nan(ests))
+apply(ests,2,mean)
+
+## saveRDS(ests, "~/Dropbox/phd/Project2/R/RESULTSbigsim220615randCens.rds")
---
+++ functions.R
@@ -0,0 +1,190 @@
+## Estimation
+em <- function(formula, data, M = 4, maxit = 100, object = NULL){
+  ## Nogle parametre til at afgøre konvergens
+  eps1 <- 1e-8
+  eps2 <- 1e-6
+  n <- length(unique(data$id))                                        # Det er antaget, at der er en id variabel til at identificere individer
+  if(is.null(object)){
+    WW <- tapply(data$W, data$id, unique)                               # Det antages at antal behandlinger er kaldt W
+    statusWW <- tapply(data$statusW, data$id, unique)                   # statusW fortæller os, hvorvidt vi har observeret faktisk antal behandlinger eller minimum
+    probs <- -diff(c(1, survfit(Surv(WW, statusWW) ~ 1)$surv))          # Marginal fordeling af W skal estimeres ikke-parametrisk men bliver initialiseret med Kaplan-Meier 
+    model <- coxph(formula, data, model = TRUE, iter = 0)
+    bas <- transform(basehaz(model), baseline = diff(c(0,hazard)))      # Vi skal bruge baseline hazard til estimation af likelihood bidrag
+  }
+  else{
+    probs <- object$probs
+    model <- object$model
+    bas <- transform(basehaz(model), baseline = diff(c(0,hazard)))      # Vi skal bruge baseline hazard til estimation af likelihood bidrag
+  }
+  # if(!is.null(beta)){
+  #   model <- coxph(formula, bigdata, model = TRUE, iter = 0, init = beta)# Cox model fittes, hvor vi ignorerer at W ikke altid er det sande W men nogle gange blot er minimum
+  # }
+  # else{
+  #   model <- coxph(formula, data, model = TRUE, iter = 0)             # Cox model fittes, hvor vi ignorerer at W ikke altid er det sande W men nogle gange blot er minimum
+  # }
+  crit <- TRUE                                                        # Et kriterie relateret til ændringen i den marginale fordeling af W
+  bigdata <- rbind(do.call("rbind", lapply(1:(M-1), function(x){      # Vi laver et datasæt, hvor individer optræder flere gange med hver af deres potentielle værdier for W
+    ko <- subset(data, statusW == 0 & W == x)                         # Vi fokuserer på de individer hvor vi ikke kender den sande værdi af W
+    do.call("rbind", lapply(x:M, function(y) transform(ko, W = y)))   # Tilføj dem til vores datasæt med alle potentielle værdier af W
+  })), subset(data, statusW == 1))                                    # Vi vil trods alt også godt have individer i vores datasæt hvor vi kender den sande værdi af W
+  bigdata <- transform(bigdata, weight = 1)                           # Vi vil lave en vægtet Cox model. Vægte er per default 1, men ændres om nogle linjers kode
+  itt <- 0                                                            # Antal iterationer initialiseres
+  while(crit & itt < maxit){
+    itt <- itt + 1
+    ## cat(itt, "\n")
+    prPrev <- probs
+    ## Likelihood bidrag starter med at være survivalfunktions bidraget. For obs med event skal senere ganges med hazard
+    bigdata <- transform(bigdata, L = predict(model, newdata = bigdata, type = "survival"))
+    ## Nedenunder ganger vi med hazard for obs med event
+    bigdata$L[bigdata$status == 1] <- bigdata$L[bigdata$status == 1] *
+      bas$baseline[sapply(bigdata$time2[bigdata$status == 1],
+                          function(x) which.min(abs(x - bas$time)))] *
+      predict(model, bigdata[bigdata$status==1,], type = "risk")
+    ## Likelihood bidrag fra samme individ og med samme værdi af W ganges sammen for at få samlet likelihood bidrag fra hvert individ per værdi af W
+    help <- aggregate(x = bigdata$L, by = list(bigdata$id, bigdata$W), FUN = prod)
+    ## Der ganges marginal fordeling af W på, da vægte per Bayes er givet ved L(W) * p(W) / sum(L(w) * p(w))
+    help <- transform(help, x = x * probs[Group.2])
+    ## Udregn sum(L(w)*p(w)) for hvert individ
+    help2 <- aggregate(help[,3], by = list(help[,1]), FUN = sum)
+    ## Udregn vægte
+    help$x <- help$x / help2$x[sapply(help$Group.1, function(x) match(1, x == help2$Group.1))]
+    ## Tilføj vægte til datasæt
+    bigdata$weight <- help$x[sapply(1:nrow(bigdata),
+                                    function(i) which(bigdata$id[i] == help[,1] & bigdata$W[i] == help[,2]))]
+    ## Fit vægtet Cox
+    model <- coxph(formula, data = bigdata, weights = weight, model = TRUE)
+    ## Skaf ny baseline hazard
+    bas <- transform(basehaz(model), baseline = diff(c(0,hazard)))
+    ## Estimer marginal fordeling af W ikke-parametrisk som gennemsnit af vægte
+    probs <- do.call("c", lapply(1:M, function(i){
+      indx <- which(bigdata$W == i)
+      sum(tapply(bigdata$weight[indx], bigdata$id[indx], unique))
+    })) / n
+    ## Error er blot absolut værdien af ændringen i estimatet af marginal fordeling af W
+    err <- abs(probs - prPrev)
+    ## Hvis ændringen er tilpas stor fortsættes estimationen
+    crit <- !all(err < (eps1 * (abs(prPrev) + eps2)))
+    ## cat(probs, "\n")
+  }
+  cat("estimation iterations: ", itt, "\n")
+  list(model = model, probs = probs, d = bigdata)                                  # Returner model objekt samt estimat af marginal fordeling af W
+}
+
+# EM algorithm variance ------------------------------------------------------------------------------------------                   
+emSE <- function(object, M = 4, maxit = Inf){
+  eps1 <- 1e-8
+  eps2 <- 1e-6
+  bigdata <- object$d
+  n <- length(unique(bigdata$id))
+  ## d <- 1 / n
+  ## d <- 1e-5
+  d <- 1 / nrow(bigdata)
+  probs <- object$probs
+  model <- object$model
+  formula <- formula(model)
+  ## w <- model$weights
+  ## X <- model.matrix(model)
+  beta <- coef(model)
+  ## time1 <- model$y[,1]
+  ## time2 <- model$y[,2]
+  ## status <- model$y[,3]
+  npar <- length(beta)
+  ## means <- m$means
+  bas <- transform(basehaz(model), baseline = diff(c(0,hazard)))
+  information <- matrix(NA, nrow = npar, ncol = npar)
+  for(i in 1:npar){
+    beta2 <- beta
+    beta2[i] <- beta2[i] + d
+    crit <- TRUE                                                        # Et kriterie relateret til ændringen i den marginale fordeling af W
+    itt <- 0
+    while(crit & itt < maxit){
+      itt <- itt + 1
+      ## cat(itt, "\n")
+      prPrev <- probs
+      bigdata <- transform(bigdata, L = predict(model, newdata = bigdata, type = "survival"))
+      ## Nedenunder ganger vi med hazard for obs med event
+      bigdata$L[bigdata$status == 1] <- bigdata$L[bigdata$status == 1] *
+        bas$baseline[sapply(bigdata$time2[bigdata$status == 1],
+                            function(x) which.min(abs(x - bas$time)))] *
+        predict(model, bigdata[bigdata$status==1,], type = "risk")
+      ## Likelihood bidrag fra samme individ og med samme værdi af W ganges sammen for at få samlet likelihood bidrag fra hvert individ per værdi af W
+      help <- aggregate(x = bigdata$L, by = list(bigdata$id, bigdata$W), FUN = prod)
+      ## Der ganges marginal fordeling af W på, da vægte per Bayes er givet ved L(W) * p(W) / sum(L(w) * p(w))
+      help <- transform(help, x = x * probs[Group.2])
+      ## Udregn sum(L(w)*p(w)) for hvert individ
+      help2 <- aggregate(help[,3], by = list(help[,1]), FUN = sum)
+      ## Udregn vægte
+      help$x <- help$x / help2$x[sapply(help$Group.1, function(x) match(1, x == help2$Group.1))]
+      ## Tilføj vægte til datasæt
+      bigdata$weight <- help$x[sapply(1:nrow(bigdata),
+                                      function(i) which(bigdata$id[i] == help[,1] & bigdata$W[i] == help[,2]))]
+      ## Fit vægtet Cox
+      model <- coxph(formula, data = bigdata, weights = weight, control = coxph.control(iter.max=0),
+                     init = beta2, model = TRUE)
+      bas <- transform(basehaz(model), baseline = diff(c(0,hazard)))
+      ## Estimer marginal fordeling af W ikke-parametrisk som gennemsnit af vægte
+      probs <- do.call("c", lapply(1:M, function(i){
+        indx <- which(bigdata$W == i)
+        sum(tapply(bigdata$weight[indx], bigdata$id[indx], unique))
+      })) / n
+      ## Error er blot absolut værdien af ændringen i estimatet af marginal fordeling af W
+      err <- abs(probs - prPrev)
+      ## Hvis ændringen er tilpas stor fortsættes estimationen
+      crit <- !all(err < (eps1 * (abs(prPrev) + eps2)))
+    }
+    ## information[i,i:npar] <- information[i:npar,i] <- 1/d * model$first[i:npar]
+    information[i,] <- 1/d * model$first
+  }
+  sqrt(diag(solve(-information)))
+  ## -information
+}
+
+ate <- function(object, timepoint){
+  d <- object$d
+  m <- object$model
+  d[, idW := as.factor(id) : as.factor(W)]
+  firsts <- d[, match(unique(idW), idW)]
+  dsub <- d[firsts,]
+  dsub[, time2 := timepoint]
+  dsub[, Y1 := predict(m, newdata = transform(dsub, treat = 1), type = "survival")]
+  dsub[, Y0 := predict(m, newdata = transform(dsub, treat = 0), type = "survival")]
+  list(ate = dsub[, sum((Y0 - Y1) * weight) / sum(weight)], 
+       risk1 = dsub[, sum((1 - Y1) * weight) / sum(weight)],
+       risk0 = dsub[, sum((1 - Y0) * weight) / sum(weight)])
+}
+
+stovlestrop <- function(formula, data, object, timepoint, nstraps = 1000, M = 4, maxit = 100, beta = NULL){
+  strapped_ests <- numeric(nstraps)
+  for(i in 1:nstraps){
+    cat("Iteration: ", i, "\n")
+    ids <- sample(unique(data$id), replace = TRUE)
+    d <- data[id %in% ids,]
+    m <- em(formula, data = d, M = M, maxit = 100, object = object)
+    cat("Cox: ", m$model$coefficients, "\n")
+    strapped_ests[i] <- ate(m, timepoint)$ate
+    cat("est: ", i, ": ", strapped_ests[i], "\n")
+  }
+  strapped_ests
+}
+
+stovlestrop_par <- function(formula, data, object, timepoint, nstraps = 1000, M = 4, maxit = 100, beta = NULL){
+  strapped_ests <- foreach(i = 1:nstraps) %dopar% {
+    cat("Iteration: ", i, "\n")
+    ids <- sample(unique(data$id), replace = TRUE)
+    d <- data[id %in% ids,]
+    m <- em(formula, data = d, M = M, maxit = 100, object = object)
+    cat("Cox: ", m$model$coefficients, "\n")
+    strapped_ests[i] <- ate(m, timepoint)$ate
+    cat("est: ", i, ": ", strapped_ests[i], "\n")
+  }
+  strapped_ests
+}
+
+cox_ate <- function(object, data, timepoint){
+  firsts <- data[, match(unique(id), id)]
+  dsub <- data[firsts,]
+  dsub[, time2 := timepoint]
+  dsub[, Y1 := predict(object, newdata = transform(dsub, treat = 1), type = "survival")]
+  dsub[, Y0 := predict(object, newdata = transform(dsub, treat = 0), type = "survival")]
+  list(ate = dsub[, mean(Y0 - Y1)], risk1 = dsub[, mean(1-Y1)], risk0 = dsub[, mean(1-Y0)])
+}
